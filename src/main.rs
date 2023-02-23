2bb4b73f278195e102ca6a53d36ab276bd076fa4
ArchUsr64 <anshulusr@gmail.com>
2023-02-23 17:28:22 +0530

    added miniquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-	println!("Hello, world!");
+use miniquad::*;
+
+struct Window {}
+
+impl Window {
+	fn new() -> Self {
+		Self {}
+	}
 }
+impl EventHandler for Window {
+	fn draw(&mut self, _ctx: &mut Context) {}
+	fn update(&mut self, _ctx: &mut Context) {}
+}
+
+fn main() {
+	miniquad::start(conf::Conf::default(), |_ctx| Box::new(Window::new()));
+}
+
