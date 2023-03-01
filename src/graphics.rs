4cbae937458342e82fb725df83ebcb328006a3bd
ArchUsr64 <anshulusr@gmail.com>
2023-03-01 17:41:56 +0530

    modified graphics to use glyphs instead of vertices

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,3 +1,4 @@
+use crate::font::Glyph;
 use miniquad::*;
 
 #[repr(C)]
@@ -22,14 +23,11 @@ pub struct Vertex {
 pub struct Stage {
 	pipeline: Pipeline,
 	bindings: Bindings,
+	vertex_buffer: Vec<Vertex>,
 }
 
 impl Stage {
 	pub fn new(ctx: &mut Context, texture_atlas: Texture) -> Self {
-		let vert = |x, y, u, v| Vertex {
-			pos: Vec2 { x, y },
-			uv: Vec2 { x: u, y: v },
-		};
 		let shader = Shader::new(
 			ctx,
 			include_str!("../shaders/vert.glsl"),
@@ -50,29 +48,44 @@ impl Stage {
 			shader,
 		);
 		let bindings = Bindings {
-			vertex_buffers: vec![Buffer::immutable(
+			vertex_buffers: vec![Buffer::immutable::<Vertex>(
 				ctx,
 				BufferType::VertexBuffer,
-				&[
-					vert(-1., -1., 0., 0.),
-					vert(1., -1., 1., 0.),
-					vert(1., 1., 1., 1.),
-					vert(-1., 1., 0., 1.),
-				],
+				&[],
 			)],
-			index_buffer: Buffer::immutable::<u16>(
-				ctx,
-				BufferType::IndexBuffer,
-				&[0, 1, 2, 0, 2, 3],
-			),
+			index_buffer: Buffer::immutable::<u16>(ctx, BufferType::IndexBuffer, &[]),
 			images: vec![texture_atlas],
 		};
-		Self { pipeline, bindings }
+		Self {
+			pipeline,
+			bindings,
+			vertex_buffer: Vec::new(),
+		}
 	}
 
 	pub fn render(&mut self, ctx: &mut Context) {
 		ctx.begin_default_pass(Default::default());
 		ctx.apply_pipeline(&self.pipeline);
+		self.bindings.vertex_buffers[0].delete();
+		self.vertex_buffer = Glyph {
+			size: Vec2::new(1., 1.),
+			pos: Vec2::new(0., 0.),
+			uv_pos: Vec2::new(0., 0.),
+			uv_size: Vec2::new(1., 1.),
+		}
+		.as_vertices()
+		.to_vec();
+		self.bindings.vertex_buffers = vec![Buffer::immutable(
+			ctx,
+			BufferType::VertexBuffer,
+			&self.vertex_buffer,
+		)];
+		self.bindings.index_buffer.delete();
+		let index_buffer: Vec<_> = [0, 1, 2, 0, 2, 3]
+			.iter()
+			.flat_map(|i| (0..self.vertex_buffer.len() as u16 / 4).map(move |j| i + j))
+			.collect();
+		self.bindings.index_buffer = Buffer::immutable(ctx, BufferType::IndexBuffer, &index_buffer);
 		ctx.apply_bindings(&self.bindings);
 		ctx.draw(0, 6, 1);
 		ctx.end_render_pass();
